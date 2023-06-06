56929a98307e7c0a84b203b8d7837c38987b8ddd
Vanonym <60972074+brayevalerien@users.noreply.github.com>
2023-06-07 00:14:14 +0200

    Added exceptions to the interpreter

--- src/stsc_interpreter.py
+++ src/stsc_interpreter.py
@@ -1,4 +1,4 @@
-from stsc_parser import Parser
+from src.stsc_parser import Parser
 
 class Interpreter:
     def __init__(self, parser: Parser) -> None:
@@ -6,7 +6,7 @@ class Interpreter:
         self.instructions = parser.tokens
         
         self.ip = 0 # instruction pointer
-        self.flags = {}
+        self.tags = {}
         
     def is_number(self, s: str) -> bool:
         try:
@@ -19,57 +19,97 @@ class Interpreter:
         while self.ip != len(self.instructions):
             cur_instruction = self.instructions[self.ip]
 
+            # number to push to the stack
             if self.is_number(cur_instruction):
                 self.stack.append(float(cur_instruction))
+                
+            # I/O instructions
             elif cur_instruction == "print":
-                print(self.stack[-1])
+                if len(self.stack) < 1:
+                    print("Cannot print, the stack is empty.")
+                    exit(-1)
+                if isinstance(self.stack[-1], float):
+                    msg = str(self.stack[-1])
+                else:
+                    msg = self.stack[-1] + " (tag)"
+                print(msg)
             elif cur_instruction == "uInput":
                 try:
                     self.stack.append(float(input("> ")))
                 except:
                     print("Invalid input. Please enter only numeric values.")
                     exit(-1)
-            elif cur_instruction == "trace":
+            elif cur_instruction == "show":
                 print(str(self.stack))
+            
+            # arithmetic instructions
             elif cur_instruction == "add":
-                # TODO check that there are at least two elements
+                if len(self.stack) < 2:
+                    print("Cannot add, the stack has less than one element.")
+                    exit(-1)
                 a, b = self.stack.pop(), self.stack.pop()
+                if isinstance(a, str):
+                    print("Cannot add " + a + " (tag) and " + b + ".")
+                    exit(-1)
+                if isinstance(b, str):
+                    print("Cannot add " + a + " and " + b + " (tag).")
+                    exit(-1)
                 self.stack.append(a + b)
             elif cur_instruction == "sub":
-                # TODO check that there are at least two elements
-                a, b = self.stack.pop(), self.stack.pop()
-                self.stack.append(a - b)
-            elif 0<len(cur_instruction) and cur_instruction[0]==">": # cur_instruction is a flag
-                #TODO handle flags
-                if (len(cur_instruction)<2):
-                    print("Flags must have at least one caracter.")
+                if len(self.stack) < 2:
+                    print("Cannot add, the stack has less than one element.")
                     exit(-1)
-                self.flags[cur_instruction[1:]] = self.ip # stores the position of the flag
-            elif self.flags.__contains__(cur_instruction):
+                a, b = self.stack.pop(), self.stack.pop()
+                if isinstance(a, str):
+                    print("Cannot sub " + a + " (tag) and " + b + ".")
+                    exit(-1)
+                if isinstance(b, str):
+                    print("Cannot sub " + a + " and " + b + " (tag).")
+                    exit(-1)
+                self.stack.append(a - b)
+                
+            # tag and jump instructions
+            elif 0<len(cur_instruction) and cur_instruction[0]==">": # cur_instruction is a tag
+                if (len(cur_instruction)<2):
+                    print("tags must have at least one caracter.")
+                    exit(-1)
+                self.tags[cur_instruction[1:]] = self.ip # stores the position of the tag
+            elif self.tags.__contains__(cur_instruction):
                 self.stack.append(cur_instruction)
             elif cur_instruction == "jump":
+                if len(self.stack) < 1:
+                    print("Cannot jump, the stack has less than one element.")
+                    exit(-1)
                 stack_top = self.stack.pop()
-                if self.flags.__contains__(stack_top):
-                    self.ip = self.flags[stack_top]
+                if self.tags.__contains__(stack_top):
+                    self.ip = self.tags[stack_top]
                 else:
-                    print("A previously defined flag must be on top of the stack when a jump instruction is executed. Current top of the stack: " + str(stack_top))
+                    print("A previously defined tag must be on top of the stack when a jump instruction is executed. Current top of the stack: " + str(stack_top))
                     exit(-1)
             elif cur_instruction == "jumpZero":
+                if len(self.stack) < 1:
+                    print("Cannot jump, the stack has less than one element.")
+                    exit(-1)
                 stack_top = self.stack.pop()
-                if self.flags.__contains__(stack_top):
+                if self.tags.__contains__(stack_top):
                     if self.stack[-1] == 0:
-                        self.ip = self.flags[stack_top]
+                        self.ip = self.tags[stack_top]
                 else:
-                    print("A previously defined flag must be on top of the stack when a jumpZero instruction is executed. Current top of the stack: " + str(stack_top))
+                    print("A previously defined tag must be on top of the stack when a jumpZero instruction is executed. Current top of the stack: " + str(stack_top))
                     exit(-1)
             elif cur_instruction == "jumpNotZero":
-                stack_top = self.stack.pop()
-                if self.flags.__contains__(stack_top):
-                    if self.stack[-1] != 0:
-                        self.ip = self.flags[stack_top]
-                else:
-                    print("A previously defined flag must be on top of the stack when a jumpNotZero instruction is executed. Current top of the stack: " + str(stack_top))
+                if len(self.stack) < 1:
+                    print("Cannot jump, the stack has less than one element.")
                     exit(-1)
+                stack_top = self.stack.pop()
+                if self.tags.__contains__(stack_top):
+                    if self.stack[-1] != 0:
+                        self.ip = self.tags[stack_top]
+                else:
+                    print("A previously defined tag must be on top of the stack when a jumpNotZero instruction is executed. Current top of the stack: " + str(stack_top))
+                    exit(-1)
+                    
+            # unrecognized instruction
             else:
                 print("Unrecognized instruction: " + cur_instruction)
                 exit(-1)
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -3,72 +3,71 @@ from unittest.mock import patch
 from io import StringIO
 from contextlib import redirect_stdout
 from src.stsc_interpreter import Interpreter
+from src.stsc_parser import Parser
 
 
 class InterpreterTests(unittest.TestCase):
-    def test1(self):
-        instructions = []
-        expected_output = ""
-        interpreter = Interpreter(instructions)
+    def run_interpreter_test(self, instructions, expected_output):
+        parser = Parser()
+        parser.parse(instructions)
+        interpreter = Interpreter(parser)
         with redirect_stdout(StringIO()) as stdout:
             interpreter.run()
             actual_output = stdout.getvalue().strip()
-        self.assertEqual(actual_output, expected_output)
-
-    def test2(self):
-        instructions = ["3", "4", "print"]
-        expected_output = "4.0"
-        interpreter = Interpreter(instructions)
-        with redirect_stdout(StringIO()) as stdout:
-            interpreter.run()
-            actual_output = stdout.getvalue().strip()
-
         self.assertEqual(actual_output, expected_output)
 
     def input_generator(self, inputs):
         for value in inputs:
             yield value
 
+    def run_interpreter_test_with_input(self, instructions, expected_output, user_inputs):
+        with patch('builtins.input', lambda _: next(self.input_generator(user_inputs))):
+            self.run_interpreter_test(instructions, expected_output)
+
+    def test1(self):
+        instructions = ""
+        expected_output = ""
+        self.run_interpreter_test(instructions, expected_output)
+
+    def test2(self):
+        instructions = "3 4 print"
+        expected_output = "4.0"
+        self.run_interpreter_test(instructions, expected_output)
+
     def test3(self):
-        instructions = ["uInput", "print"]
+        instructions = "uInput print"
         expected_output = "2.5"
         user_inputs = ["2.5\n"]
-        interpreter = Interpreter(instructions)
-        with redirect_stdout(StringIO()) as stdout, \
-                patch('builtins.input', lambda _: next(self.input_generator(user_inputs))):
-            interpreter.run()
-            actual_output = stdout.getvalue().strip()
-        self.assertEqual(actual_output, expected_output)
+        self.run_interpreter_test_with_input(instructions, expected_output, user_inputs)
 
     def test4(self):
-        instructions = ["uInput"]
+        instructions = "uInput"
         expected_output = "Invalid input. Please enter only numeric values."
         user_inputs = ["invalid\n"]
-        interpreter = Interpreter(instructions)
-        with redirect_stdout(StringIO()) as stdout, \
-                patch('builtins.input', lambda _: next(self.input_generator(user_inputs))), \
-                self.assertRaises(SystemExit):
-            interpreter.run()
-            actual_output = stdout.getvalue().strip()
-            self.assertEqual(actual_output, expected_output)
+        with self.assertRaises(SystemExit):
+            self.run_interpreter_test_with_input(instructions, expected_output, user_inputs)
 
     def test5(self):
-        instructions = ["3", "4", "add", "print"]
+        instructions = "3 4 add print"
         expected_output = "7.0"
-        interpreter = Interpreter(instructions)
-        with redirect_stdout(StringIO()) as stdout:
-            interpreter.run()
-            actual_output = stdout.getvalue().strip()
-        self.assertEqual(actual_output, expected_output)
+        self.run_interpreter_test(instructions, expected_output)
 
     def test6(self):
-        instructions = ["10", "5", "sub", "print"]
+        instructions = "10 5 sub print"
         expected_output = "-5.0"
-        interpreter = Interpreter(instructions)
-        with redirect_stdout(StringIO()) as stdout:
-            interpreter.run()
-            actual_output = stdout.getvalue().strip()
-        self.assertEqual(actual_output, expected_output)
+        self.run_interpreter_test(instructions, expected_output)
+
+    def test7(self):
+        instructions = "5 >loop -1 add loop jumpNotZero print"
+        expected_output = "0"
+        self.run_interpreter_test(instructions, expected_output)
+
+    def test7(self):
+        instructions = "5 >loop -1 add 0 jumpNotZero"
+        expected_output = "A previously defined tag must be on top of the stack when a jumpNotZero instruction is executed. Current top of the stack: 0.0"
+        with self.assertRaises(SystemExit):
+            self.run_interpreter_test(instructions, expected_output)
+
 
 if __name__ == '__main__':
     unittest.main()
